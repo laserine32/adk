708ac3905ad80f4a28e01848cb4bea055999c56b
laserine32 <laserinobatbatuk32@gmail.com>
2026-06-21 15:39:24 +0700

    fixing check api

--- src/app/api/check/[id]/route.ts
+++ src/app/api/check/[id]/route.ts
@@ -2,11 +2,11 @@ import { checkOnlineKomik } from "@/db/queries/add-komik";
 import { NextRequest, NextResponse } from "next/server";
 
 type ChekApiIdProps = {
-	params: Promise<{ id: number }>;
+	params: Promise<{ id: string }>;
 };
 
 export async function GET(request: NextRequest, { params }: ChekApiIdProps) {
 	const { id } = await params;
-	const available = await checkOnlineKomik(id);
+	const available = await checkOnlineKomik(Number(id));
 	return NextResponse.json({ available }, { status: 200 });
 }
